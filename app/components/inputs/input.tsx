a3fdf32a631ddd885147b3dbde1a7dc142bba8b1
nuhptr <nugrahaadi733@gmail.com>
2023-09-16 01:52:21 +0700

    comp: update code

--- app/components/inputs/input.tsx
+++ app/components/inputs/input.tsx
@@ -13,20 +13,10 @@ interface InputProps {
   disabled?: boolean
 }
 
-const Input: React.FC<InputProps> = ({
-  label,
-  id,
-  type,
-  required,
-  register,
-  errors,
-  disabled,
-}) => {
+const Input: React.FC<InputProps> = ({ label, id, type, required, register, errors, disabled }) => {
   return (
     <div>
-      <label
-        htmlFor={id}
-        className='block text-sm font-medium leading-6 text-gray-900'>
+      <label htmlFor={id} className='block text-sm font-medium leading-6 text-gray-900'>
         {label}
       </label>
       <div className='mt-2'>
@@ -37,7 +27,7 @@ const Input: React.FC<InputProps> = ({
           disabled={disabled}
           {...register(id, { required })}
           className={clsx(
-            `form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6`,
+            `form-input block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6`,
             errors[id] && 'focus:ring-rose-500',
             disabled && 'opacity-50 cursor-default'
           )}
