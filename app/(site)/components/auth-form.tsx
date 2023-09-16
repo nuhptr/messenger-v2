3f85fd929b4c9334f77a87187a3be8538a28e409
nuhptr <nugrahaadi733@gmail.com>
2023-09-16 18:42:15 +0700

    site: add auth component

--- app/(site)/components/auth-form.tsx
+++ app/(site)/components/auth-form.tsx
@@ -2,16 +2,19 @@
 
 import { useCallback, useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import { BsGithub, BsGoogle } from 'react-icons/bs'
 
 import Input from '@/app/components/inputs/input'
+import Button from '@/app/components/button'
+import AuthSocialButton from './auth-social-button'
 
 type Variant = 'LOGIN' | 'REGISTER'
 
 const AuthForm = () => {
   const [variant, setVariant] = useState<Variant>('LOGIN')
-  const [loading, setIsLoading] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
-  const toogleVariant = useCallback(() => {
+  const toggleVariant = useCallback(() => {
     if (variant === 'LOGIN') setVariant('REGISTER')
     else setVariant('LOGIN')
   }, [variant])
@@ -50,9 +53,15 @@ const AuthForm = () => {
     <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
       <div className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
         {/* Form */}
-        <form className='space-y-8' onSubmit={handleSubmit(onSubmit)}>
+        <form className='space-y-4' onSubmit={handleSubmit(onSubmit)}>
           {variant === 'REGISTER' && (
-            <Input id='name' label='Name' register={register} errors={errors} />
+            <Input
+              id='name'
+              label='Name'
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
           )}
           <Input
             id='email'
@@ -60,6 +69,7 @@ const AuthForm = () => {
             type='email'
             register={register}
             errors={errors}
+            disabled={isLoading}
           />
           <Input
             id='password'
@@ -67,8 +77,37 @@ const AuthForm = () => {
             type='password'
             register={register}
             errors={errors}
+            disabled={isLoading}
           />
+          <div>
+            <Button disabled={isLoading} fullWidth type='submit'>
+              {variant === 'LOGIN' ? 'Login' : 'Register'}
+            </Button>
+          </div>
         </form>
+
+        <div className='mt-6'>
+          <div className='relative'>
+            <div className='absolute inset-0 flex items-center'>
+              <div className='w-full border-t border-gray-300' />
+            </div>
+            <div className='relative flex justify-center text-sm'>
+              <span className='bg-white px-2 text-gray-500'>Or continue with</span>
+            </div>
+          </div>
+
+          <div className='mt-6 flex gap-2'>
+            <AuthSocialButton icon={BsGithub} onClick={() => socialAction('github')} />
+            <AuthSocialButton icon={BsGoogle} onClick={() => socialAction('google')} />
+          </div>
+        </div>
+
+        <div className='flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500'>
+          <div>{variant === 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'}</div>
+          <div className='underline cursor-pointer' onClick={toggleVariant}>
+            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+          </div>
+        </div>
       </div>
     </div>
   )
