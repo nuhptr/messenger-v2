9d729ceca3851f1dc134719e71678b9c3979316c
nuhptr <nugrahaadi733@gmail.com>
2023-09-18 18:59:22 +0700

    hooks: change name file

--- app/hooks/use-routes.ts
+++ app/hooks/use-routes.ts
@@ -5,7 +5,7 @@ import { HiChat } from 'react-icons/hi'
 import { HiArrowLeftOnRectangle, HiUsers } from 'react-icons/hi2'
 import { signOut } from 'next-auth/react'
 
-import useConversation from './useConversation'
+import useConversation from './use-conversation'
 
 export default function useRoutes() {
   const pathname = usePathname()
