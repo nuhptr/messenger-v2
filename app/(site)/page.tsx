8fb0fe79f1d2078c0bd080152f630f3960511cac
nuhptr <nugrahaadi733@gmail.com>
2023-09-18 18:57:08 +0700

    app: change to auth-header

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,24 +1,10 @@
-import Image from 'next/image'
-
 import AuthForm from './components/auth-form'
+import AuthHeader from './components/auth-header'
 
 export default function Home() {
   return (
     <div className='min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100'>
-      <div className='sm:mx-auto sm:w-full sm:max-w-md'>
-        <Image
-          alt='Messenger Logo'
-          src={'/images/logo.png'}
-          height={48}
-          width={48}
-          className='mx-auto w-auto'
-        />
-        <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
-          Sign in to your account
-        </h2>
-      </div>
-
-      {/* Authform */}
+      <AuthHeader />
       <AuthForm />
     </div>
   )
