edb1a31c62b4e9e5afcb1ea34b2171be66afb1cd
nuhptr <nugrahaadi733@gmail.com>
2023-09-20 15:30:15 +0700

    comp: update & add

--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -1,10 +1,15 @@
 import DesktopSidebar from './desktop-sidebar'
 import MobileFooter from './mobile-footer'
 
+import getCurrentUser from '@/app/actions/get-current-user'
+
 export default async function Sidebar({ children }: { children: React.ReactNode }) {
+  const currentUser = await getCurrentUser()
+
   return (
     <div className='h-full'>
-      <DesktopSidebar />
+      {/* ! exclamation mark is ensuring that your data is not null */}
+      <DesktopSidebar currentUser={currentUser!} />
       <MobileFooter />
       <main className='lg:pl-20 h-full'>{children}</main>
     </div>
